e09d2ed49de720b8aa4fb2a3429f203339c91d70
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2007-01-08 15:52:06 +0000

    昨年度英語版の画像を追加.
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/trunk@60 0cb0e2da-76ef-49b7-9531-4241df5af7e9

---
+++ images/images_en/rollover.js
@@ -0,0 +1,21 @@
+var preloadFlag = false;
+function newImage(arg) {
+	if (document.images) {
+		rslt = new Image();
+		rslt.src = arg;
+		return rslt;
+	}
+}
+function changeImages() {
+	if (document.images && (preloadFlag == true)) {
+		for (var i=0; i<changeImages.arguments.length; i+=2) {
+			document[changeImages.arguments[i]].src = changeImages.arguments[i+1];
+		}
+	}
+}
+function preloadImages() {
+	if (document.images) {
+		over_cl = newImage(/*URL*/'../../img/n_cl_on.gif');
+		preloadFlag = true;
+	}
+}
