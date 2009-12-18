13c251381acf4fe8b441348ae9cd049254a0262d
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2009-12-18 10:02:00 +0000

    コースホームの html, CSS のオリジナル(12/16)を追加.
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@1336 0cb0e2da-76ef-49b7-9531-4241df5af7e9

---
+++ original_html/test.js
@@ -0,0 +1,31 @@
+$(function(){
+    method2();
+    mouse();
+});
+  
+  function mouse(){
+    //  $("img.rollover").mouseover(function(){
+      //    $(this).attr("src",$(this).attr("src").replace(/(\.)^(.+)(\.[a-z]+)$/, "$1$2_on$3"));
+     // }).mouseout(function(){
+       //   $(this).attr("src",$(this).attr("src").replace(/(\.)^(.+)_on(\.[a-z]+)$/, "$1$2$3"));
+      // })
+      $("img[src='./navi/n02lit.gif']").onmouseover(function(){
+          $(this).attr("src","./navi/n02lit_on.gif");
+      }).onmouseout(function(){
+          $(this).attr("src","./navi/n02lit.gif");
+      })
+  };
+  
+  // 方法2
+  function method2() {
+      $("#method2 a").each(function(){
+	  var weight = $(this).css("font-weight");
+	  // $(this).text(weight); // 分岐に使う出力の確認(ブラウザ依存の可能性がある)
+	  if (weight == "400") {
+	      // 訪問済みのものにあたる
+      // 単純に $("#method2 a:visited") としたいが動かないみたい
+	      $(this).children("img").attr("src", "n01kyoiku-in.gif");
+	      $(this).children("img").attr("alt", "Visited");
+	  } 
+      });
+  };
