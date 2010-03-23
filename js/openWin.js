db851fef3dfc722f6c8f772f0191d0c8f7c98ae8
masanori <masanori@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2010-03-23 07:50:18 +0000

    エラー修正
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@1775 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/openWin.js
+++ js/openWin.js
@@ -2,7 +2,7 @@
  
 function openWin(theURI){
  PopUpWin=window.open(theURI,'theWin','scrollbars=0,
- width=250,height=320,resizable=1,directories=0,
- toolbar=0,status=1,location=0');
+ width=250, height=320, resizable=1, directories=0,
+ toolbar=0, status=1, location=0');
  PopUpWin.focus();
 }
