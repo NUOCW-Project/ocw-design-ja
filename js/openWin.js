f2a603c0fd6f795c1421a266cfbe525a0e054d3a
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2010-03-23 07:45:23 +0000

    typo 修正.
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@1773 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/openWin.js
+++ js/openWin.js
@@ -1,6 +1,6 @@
 //一分間授業紹介の動画へのリンクをポップアップウィンドウで開く
  
-functon openWin(theURI){
+function openWin(theURI){
  PopUpWin=window.open(theURI,'theWin','scrollbars=0,
  width=250,height=320,resizable=1,directories=0,
  toolbar=0,status=1,location=0');
