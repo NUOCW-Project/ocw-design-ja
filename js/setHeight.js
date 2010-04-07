9598c2efbc3fb561ab8446b11a31d9aedab5e22a
alla <alla@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2010-04-07 16:48:38 +0000

    js 修正test
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@2618 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/setHeight.js
+++ js/setHeight.js
@@ -1,6 +1,6 @@
 function method3() {	
 	var H;
-	H = $("#notice").outerHeight() + $("#press").outerHeight() -5;
+	H = $("#notice").outerHeight() + $("#press").outerHeight() -6;
 	var cssValue = $("#access div").css("height", H + "px");
 
 	H = H - 30;
