6ba0e0a5094f97e41abe566cdf93d86b9afd5660
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2010-10-31 13:31:24 +0000

    Merged revisions 4264,4268 via svnmerge from file:///ocw/svn/design-rep/ocw-design/branches/2010
    ........
      r4264 | adachi | 2010-10-31 18:30:15 +0900 (日, 31 10月 2010) | 1 line
      
      勉強会の内容に基づいて JS を修正.
    ........
      r4268 | adachi | 2010-10-31 22:28:54 +0900 (日, 31 10月 2010) | 3 lines
      
      アクセスランキングの高さ調整の JS を修正.
      window の onload 時でなく、jquery で DOM 解析後に実行.
      タブを空白に置換.
    ........
    
    
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/tags/release-2.0-4136@4269 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/toggle.js
+++ js/toggle.js
@@ -3,20 +3,18 @@ $(function(){
 
   // 右上の AKF 検索の中身をクリック時にクリア
   $("#searchbox").focus(function(){
-	  $(this).val("").css("color","#000");
+    $(this).val("").css("color","#000");
   })
 });
 
-$(window).resize(function(){
-  toggleStaff();
-});
+$(window).resize(toggleStaff);
 
 function toggleStaff() {
   var width = $("body").width();
-  $("#width").text(width);
   if (width > 1125) {
     $(".main_back_adachi").show();
   } else {
     $(".main_back_adachi").hide();
   } 
 }
+
