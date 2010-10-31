403ea0b0ebc60ce0512a235d633426a587e0e533
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2010-10-31 13:28:54 +0000

    アクセスランキングの高さ調整の JS を修正. window の onload 時でなく、jquery で DOM 解析後に実行. タブを空白に置換.
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@4268 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/setHeight.js
+++ js/setHeight.js
@@ -1,18 +1,19 @@
-window.onload = function() {	
-	var H;
-	H = $("#notice").outerHeight() + $("#press").outerHeight() -8;
-	var cssValue = $("#access div").css("height", H + "px");
+// DOM 解析後にランキング表示を調整.
+$(function(){ 
+  var H;
+  H = $("#notice").outerHeight() + $("#press").outerHeight() - 8;
+  $("#access div").css("height", H + "px");
 
-	H = H - 30;
-	var G = 0;
-	var i = 0;
+  H = H - 30;
+  var G = 0;
+  var i = 0;
 
-	while ((G <= H) && (i<=20)){
-		G = G + $("#no" + i).outerHeight(true);
-		i++;
-	}
-		i--;
+  while ((G <= H) && (i <= 20)){
+    G = G + $("#no" + i).outerHeight(true);
+    i++;
+  }
+  i--;
 
-	G = G - $("#no" + i).outerHeight(true);
-	var cssValue = $("#rank_table").css("clip", "rect(0px, auto, " + G + "px, auto)");
-}
+  G = G - $("#no" + i).outerHeight(true);
+  $("#rank_table").css("clip", "rect(0px, auto, " + G + "px, auto)");
+});
--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,5 @@
 $(function(){
   toggleStaff();
-  setRankingHeight();
 
   // 右上の AKF 検索の中身をクリック時にクリア
   $("#searchbox").focus(function(){
@@ -19,21 +18,3 @@ function toggleStaff() {
   } 
 }
 
-function setRankingHeight() {  
-  var H;
-  H = $("#notice").outerHeight() + $("#press").outerHeight() - 8;
-  $("#access div").css("height", H + "px");
-
-  H = H - 30;
-  var G = 0;
-  var i = 0;
-
-  while ((G <= H) && (i <= 20)){
-    G = G + $("#no" + i).outerHeight(true);
-    i++;
-  }
-  i--;
-
-  G = G - $("#no" + i).outerHeight(true);
-  $("#rank_table").css("clip", "rect(0px, auto, " + G + "px, auto)");
-}
