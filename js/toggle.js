21d76dc687d27c1de85e6f2e43d96f9a86c12909
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2010-10-31 09:30:15 +0000

    勉強会の内容に基づいて JS を修正.
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@4264 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/toggle.js
+++ js/toggle.js
@@ -1,22 +1,39 @@
 $(function(){
   toggleStaff();
+  setRankingHeight();
 
   // 右上の AKF 検索の中身をクリック時にクリア
   $("#searchbox").focus(function(){
-	  $(this).val("").css("color","#000");
+    $(this).val("").css("color","#000");
   })
 });
 
-$(window).resize(function(){
-  toggleStaff();
-});
+$(window).resize(toggleStaff);
 
 function toggleStaff() {
   var width = $("body").width();
-  $("#width").text(width);
   if (width > 1125) {
     $(".main_back_adachi").show();
   } else {
     $(".main_back_adachi").hide();
   } 
+}
+
+function setRankingHeight() {  
+  var H;
+  H = $("#notice").outerHeight() + $("#press").outerHeight() - 8;
+  $("#access div").css("height", H + "px");
+
+  H = H - 30;
+  var G = 0;
+  var i = 0;
+
+  while ((G <= H) && (i <= 20)){
+    G = G + $("#no" + i).outerHeight(true);
+    i++;
+  }
+  i--;
+
+  G = G - $("#no" + i).outerHeight(true);
+  $("#rank_table").css("clip", "rect(0px, auto, " + G + "px, auto)");
 }
