c1efc6548a51922486e03416b51bb9f5a890b251
masanori <masanori@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-07-15 13:07:39 +0000

    留学生の声編集
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@5704 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/switchFlash.js
+++ js/switchFlash.js
@@ -2,12 +2,13 @@
   // exid : 切り替えるdiv要素のid
   // url : 再生させる動画のパス
   // title : 動画のタイトル 
-function switchFlash(exid , url, title) {
+function switchFlash(url, title) {
+    // divの削除
     var target = document.getElementById('top_box');
     target.removeChild(target.firstChild);
     // divエレメントの生成
     var element = document.createElement('div');
-    element.id = exid;
+    element.id = 'top_video';
     element.innerHTML = '\
 <objct classid="clsid:D27CDB6E- AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="317" height="400">\
     <param name="movie" value="player.swf">\
