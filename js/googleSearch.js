4ef73bbaf2fecda44f601269118a2c0b58114096
zhongyang <zhongyang@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-07-25 13:51:47 +0000

    google search
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@5726 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/googleSearch.js
+++ js/googleSearch.js
@@ -1,6 +1,8 @@
-     google.load('search', '1', {language : 'ja', style : google.loader.themes.GREENSKY});
+    google.load('search', '1', {language : 'ja', style : google.loader.themes.GREENSKY});
 google.setOnLoadCallback(function() {
 	var customSearchControl = new google.search.CustomSearchControl('001246554537135396434:mvaxczobgbe');
 	customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-	customSearchControl.draw('cse');
+	var options = new google.search.DrawOptions();
+	options.enableSearchboxOnly("http://google.com/cse?cx=001246554537135396434:mvaxczobgbe");    
+	customSearchControl.draw('cse-search-form', options);
     }, true);
