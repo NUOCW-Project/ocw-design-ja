f899912865eb68476e12b865b9364f1b8bda56be
masanori <masanori@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-07-25 07:38:55 +0000

    留学生の声編集
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@5724 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/switchFlash.js
+++ js/switchFlash.js
@@ -12,7 +12,7 @@ function switchFlash(url, title) {
     var element = document.createElement('div');
     element.id = 'interview_video_frame';
     element.innerHTML = '\
-<div id="interview_title"></div>\
+<div id="interview_title">' + title + '</div>\
 <div id="interviw_video">
 <objcet classid="clsid:D27CDB6E- AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="317" height="400">\
     <param name="movie" value="player.swf">\
