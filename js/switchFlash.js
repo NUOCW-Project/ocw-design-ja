349fd1e9058f37ecf0b8cecf19cbee91fac6d3f3
masanori <masanori@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-07-25 07:35:40 +0000

    留学生の声編集
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@5723 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/switchFlash.js
+++ js/switchFlash.js
@@ -10,15 +10,18 @@ function switchFlash(url, title) {
     }
     // divエレメントの生成
     var element = document.createElement('div');
-    element.id = 'top_video';
+    element.id = 'interview_video_frame';
     element.innerHTML = '\
+<div id="interview_title"></div>\
+<div id="interviw_video">
 <objcet classid="clsid:D27CDB6E- AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="317" height="400">\
     <param name="movie" value="player.swf">\
     <param name="quality" value="high">\
     <param name="wmode" value="transparent">\
     <param name="FlashVars" value="FLV_URL=' + url + '">\
     <embed src="./player.swf" FlashVars="FLV_URL=' + url + '" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" wmode="transparent" width="317" height="400"></embed>\
-    </object>';
+</object>\
+</div>';
     // divエレメントの追加
     target.appendChild(element);
 }
