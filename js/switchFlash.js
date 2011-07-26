650009e8f6da73705590d3734a48181d1b02aed5
masanori <masanori@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-07-26 00:14:29 +0000

    留学生の声
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@5733 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/switchFlash.js
+++ js/switchFlash.js
@@ -10,10 +10,10 @@ function switchFlash(url, title) {
     }
     // divエレメントの生成
     var element = document.createElement('div');
-    element.id = 'interview_video_frame';
+    element.id = 'top_video';
     element.innerHTML = '\
 <div id="interview_title">' + title + '</div>\
-<div id="interviw_video">\
+<div id="interview_video">\
 <objcet classid="clsid:D27CDB6E- AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="317" height="400">\
     <param name="movie" value="player.swf">\
     <param name="quality" value="high">\
