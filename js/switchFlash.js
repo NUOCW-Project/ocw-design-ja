175d63933e389096324b40e57e5a52f9478e2083
masanori <masanori@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-08-19 08:51:53 +0000

    留学生の声、動画のタイトルを画像で表示
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@5870 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/switchFlash.js
+++ js/switchFlash.js
@@ -12,7 +12,7 @@ function switchFlash(url, title, img) {
     var element = document.createElement('div');
     element.id = 'interview_video_frame';
     element.innerHTML = '\
-<div id="interview_title"><img src="interview_title' + img + '.png" alt="' + title + '"></div>\
+<div id="interview_title"><img src="./images/common/student/interview_title_' + img + '.png" alt="' + title + '"></div>\
 <div id="interview_video">\
 <objcet classid="clsid:D27CDB6E- AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="302" height="272">\
     <param name="movie" value="player.swf">\
