5437b85a1a1b2e5cecee70a8e9ba23581d5ba411
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-08-29 02:08:37 +0000

    Merged revisions 5668,5907-5908,5919-5926 via svnmerge from file:///ocw/svn/design-rep/ocw-design/branches/2010
    ........
      r5668 | masanori | 2011-07-11 16:03:10 +0900 (月, 11  7月 2011) | 1 line
      
      留学生の声
    ........
      r5907 | adachi | 2011-08-25 17:26:07 +0900 (木, 25  8月 2011) | 1 line
      
      留学生の声のビデオ種類切り切り替えのJS関数 seltab() の適用の仕方を変更.
    ........
      r5908 | adachi | 2011-08-25 17:31:48 +0900 (木, 25  8月 2011) | 1 line
      
      fix a typo.
    ........
      r5919 | adachi | 2011-08-28 00:04:35 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声のページ切り替えタブを修正.
    ........
      r5920 | adachi | 2011-08-28 00:07:00 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声のページ切り替えタブを修正.
    ........
      r5921 | adachi | 2011-08-28 00:11:34 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声のページ切り替えタブを修正.
    ........
      r5922 | adachi | 2011-08-28 00:16:44 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声のスタイル調整
    ........
      r5923 | adachi | 2011-08-28 00:18:17 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声のスタイル調整
    ........
      r5924 | adachi | 2011-08-28 00:25:10 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声のスタイル調整：コメント付加.
    ........
      r5925 | adachi | 2011-08-28 00:39:59 +0900 (日, 28  8月 2011) | 1 line
      
      留学生の声：img に alt 付加、a の閉じ忘れ修正.
    ........
      r5926 | adachi | 2011-08-28 00:42:03 +0900 (日, 28  8月 2011) | 1 line
      
      r5925 の続き.
    ........
    
    
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/tags/release-2.1-5179@5937 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/seltab.js
+++ js/seltab.js
@@ -1,14 +1,16 @@
+// we first show "1. About Japan".
+seltab('theme', 3, 1);
 
 // bpref = tab body prefix
-    function seltab(bpref, id_max, selected) {
-	 for (i = 0; i <= id_max; i++) {
-	     if (! document.getElementById(bpref + i)) continue;
-	     if (i == selected) {
-		 document.getElementById(bpref + i).style.visibility = "visible";
-		 document.getElementById(bpref + i).style.position = "";
-	     } else {
-		 document.getElementById(bpref + i).style.visibility = "hidden";
-		 document.getElementById(bpref + i).style.position = "absolute";
-	     }
-	 }
-     }
+function seltab(bpref, id_max, selected) {
+  for (i = 0; i <= id_max; i++) {
+    if (! document.getElementById(bpref + i)) continue;
+    if (i == selected) {
+      document.getElementById(bpref + i).style.visibility = "visible";
+      document.getElementById(bpref + i).style.position = "";
+    } else {
+      document.getElementById(bpref + i).style.visibility = "hidden";
+      document.getElementById(bpref + i).style.position = "absolute";
+    }
+  }
+}
