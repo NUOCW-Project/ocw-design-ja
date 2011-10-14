2d635132551a28255f4c1472d7297edbd10e26c7
adachi <adachi@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-10-14 07:34:43 +0000

    AKF 検索を取り外したので検索欄のクリアは不要になった
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@6361 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/toggle.js
+++ js/toggle.js
@@ -1,14 +1,3 @@
-$(function(){
-  toggleStaff();
-
-  // 右上の AKF 検索の中身をクリック時にクリア
-  $("#searchbox").focus(function(){
-    $(this).val("").css("color","#000");
-  })
-});
-
-$(window).resize(toggleStaff);
-
 function toggleStaff() {
   var width = $("body").width();
   if (width > 1125) {
@@ -18,3 +7,5 @@ function toggleStaff() {
   } 
 }
 
+$(toggleStaff);
+$(window).resize(toggleStaff);
