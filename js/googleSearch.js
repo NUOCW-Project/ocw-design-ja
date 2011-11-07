b8d9fd91f1dd47f8959460cc8282392db61688ea
mikito <mikito@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2011-11-07 05:12:36 +0000

    google検索
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@6397 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/googleSearch.js
+++ js/googleSearch.js
@@ -1,5 +1,5 @@
   google.load('search', '1', {language : 'ja', style : google.loader.themes.GREENSKY});
-  google.setOnLoadCallback(function() {
+google.setOnLoadCallback(function(), {
     var customSearchControl = new google.search.CustomSearchControl('001246554537135396434:mvaxczobgbe');
     customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
     var options = new google.search.DrawOptions();
