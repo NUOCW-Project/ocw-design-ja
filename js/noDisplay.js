9f9b28f907366831b8459761c9137189af3c3e3c
yamazaki-m <yamazaki-m@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2013-02-04 16:07:58 +0000

    TOPICS 2011 の改善
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@7413 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/noDisplay.js
+++ js/noDisplay.js
@@ -1,31 +1,38 @@
-function Display(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
+$(document).ready(function(){
+	    $(".intro").css("display","none");
+	    $(".intro").eq(0).("display","inherit");
 
-    if(obj)
-	obj.style.display="inherit";
-}
+   $(".title").mouseover(function(){
+	var index = $(".title").index(this);
 
-function noDisplay(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
+	if(index != 0){
+	    index = index + 1;
 
-    if(obj)
-	obj.style.display="none";
-}
+	    $(".intro").css("display","none");
+	    $(".intro").eq(index).css("display","inherit");
 
-function Color(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
+	    $(".blue_bar").css("display","none");
+	    $(".blue_bar").eq(index).css("display","inherit");
 
-    if(obj)
-	obj.style.color="#99CCCC";
-}
+	    $(".detail").css("display","none");
+	    $(".detail").eq(index).css("display","inherit");
 
-function reColor(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
+	    $(".tp_title").css("color","");
+	    $(".tp_title").eq(index).css("color","#99CCCC");
+	}
 
-    if(obj)
-	obj.style.color="";
-}
+	if(index == 0){
+		$(".articles_title").click(function(){
+		    $(".intro").css("display","none");
+		    $(".intro").eq(index).css("display","none");
+
+		    $(".blue_bar").css("display","none");
+
+		    $(".detail").css("display","none");
+
+		    $(".tp_title").css("color","");
+		});	
+	}
+    });
+
+});
