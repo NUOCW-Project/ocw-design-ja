442b6cc23efc0efde1e880332565d1d1a64fbe89
yamazaki-m <yamazaki-m@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2013-02-14 05:33:05 +0000

    noDisplay.js edited
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@7425 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/noDisplay.js
+++ js/noDisplay.js
@@ -1,3 +1,39 @@
+$(document).ready(function(){
+
+    $(".title").mouseover(function(){
+	var index = $(".title").index(this);
+
+	if(index != 0){
+	    $(".intro").css("display","none");
+	    $(".intro").eq(index).css("display","inherit");
+
+	    $(".detail").css("display","none");
+	    $(".detail").eq(index).css("display","inherit");
+
+	    $(".blue_bar").css("display","none");
+	    $(".blue_bar").eq(index).css("display","inherit");
+
+	    $(".tptitle").css("color","");
+	    $(".tptitle").eq(index).css("color","#99CCCC");
+	}
+
+	if(index == 0){
+	    $(".title").eq(index).click(function()
+	    $(".intro").css("display","none");
+	    $(".intro").eq(index).css("display","inherit");
+
+	    $(".detail").css("display","none");
+	    $(".detail").eq(index).css("display","inherit");
+
+	    $(".blue_bar").css("display","none");
+	    $(".blue_bar").eq(index).css("display","inherit");
+
+	    $(".tptitle").css("color","");
+	    $(".tptitle").eq(index).css("color","#99CCCC");
+	    });
+	}
+});
+
 function Display(id){
     var obj=document.all && document.all(id)
 	|| document.getElementById && document.getElementById(id);
