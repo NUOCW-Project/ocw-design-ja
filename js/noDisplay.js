a92ebc07065a274e0172c013d02b89d59e6c755e
yamazaki-m <yamazaki-m@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2013-02-14 06:23:03 +0000

    special_contents2.tpl edited
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@7437 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/noDisplay.js
+++ js/noDisplay.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $(".art_title").mouseover(function(){
 	var index = $(".art_title").index(this);
 
-	if(index != 0){
 	    $(".intro").css("display","none");
 	    $(".intro").eq(index).css("display","inherit");
 
@@ -15,23 +14,6 @@ $(document).ready(function(){
 
 	    $(".tp_title").css("color","");
 	    $(".tp_title").eq(index).css("color","#99CCCC");
-	}
-
-	if(index == 0){
-	    $(".art_title").eq(index).click(function(){
-		    $(".intro").css("display","none");
-		    $(".intro").eq(index).css("display","inherit");
-
-		    $(".detail").css("display","none");
-		    $(".detail").eq(index).css("display","inherit");
-
-		    $(".blue_bar").css("display","none");
-		    $(".blue_bar").eq(index).css("display","inherit");
-
-		    $(".tp_title").css("color","");
-		    $(".tp_title").eq(index).css("color","#99CCCC");
-	    });	
-	}
     });
 
 });
