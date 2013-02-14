c8bbe4cb02f78e2a45a376dc8ea9b32e2c256c6b
yamazaki-m <yamazaki-m@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2013-02-14 06:08:28 +0000

    noDisplay.js edited
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@7431 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/noDisplay.js
+++ js/noDisplay.js
@@ -33,35 +33,3 @@ $(document).ready(function(){
 	    });
 	}
 });
-
-function Display(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
-
-    if(obj)
-	obj.style.display="inherit";
-}
-
-function noDisplay(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
-
-    if(obj)
-	obj.style.display="none";
-}
-
-function Color(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
-
-    if(obj)
-	obj.style.color="#99CCCC";
-}
-
-function reColor(id){
-    var obj=document.all && document.all(id)
-	|| document.getElementById && document.getElementById(id);
-
-    if(obj)
-	obj.style.color="";
-}
