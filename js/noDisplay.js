4d259108f64db8f69624933d0139efec477be935
yamazaki-m <yamazaki-m@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2013-02-21 07:36:11 +0000

    noDisplay.js backup deleted
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010@7444 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/noDisplay.js
+++ js/noDisplay.js
@@ -1,55 +1,31 @@
-$(document).ready(function(){
-
-    $(".intro").css("display","none");
-    $(".intro:first").css("display","inherit");
-
-    $(".detail").css("display","none");
-
-    $(".art_title").mouseover(function(){
-	var index = $(".art_title").index(this);
-
-	if(index != 0){
-	    $(".intro").css("display","none");
-	    $(".intro").eq(index).css("display","inherit");
-
-	    $(".blue_bar").css("display","none");
-	    $(".blue_bar").eq(index - 1).css("display","inherit");
-
-	    $(".tp_title").css("color","");
-	    $(".tp_title").eq(index - 1).css("color","#99CCCC");
-
-	    $(".art_title").eq(index).click(function(){
-		    $(".detail").css("display","none");	
-		    $(".detail").eq(index - 1).css("display","inherit");
-	    });
-	}
-
-	if(index == 0){
-	    $(".art_title").eq(index).click(function(){
-		    $(".intro").css("display","none");
-		    $(".intro").eq(index).css("display","inherit");
-
-		    $(".blue_bar").css("display","none");
-		    $(".tp_title").css("color","");
-		    $(".detail").css("display","none");
- 	    });
-	}
-    });
-
-});
-
 function Display(id){
     var obj=document.all && document.all(id)
-        || document.getElementById && document.getElementById(id);
+	|| document.getElementById && document.getElementById(id);
 
     if(obj)
-        obj.style.display="inherit";
+	obj.style.display="inherit";
 }
 
 function noDisplay(id){
     var obj=document.all && document.all(id)
-        || document.getElementById && document.getElementById(id);
+	|| document.getElementById && document.getElementById(id);
 
     if(obj)
-        obj.style.display="none";
+	obj.style.display="none";
+}
+
+function Color(id){
+    var obj=document.all && document.all(id)
+	|| document.getElementById && document.getElementById(id);
+
+    if(obj)
+	obj.style.color="#99CCCC";
+}
+
+function reColor(id){
+    var obj=document.all && document.all(id)
+	|| document.getElementById && document.getElementById(id);
+
+    if(obj)
+	obj.style.color="";
 }
