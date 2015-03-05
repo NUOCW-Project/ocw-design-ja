a918b538e5e3783c5f03cf86b63daee3c567dd28
takaichi-d <takaichi-d@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2015-03-05 07:32:13 +0000

    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/2010u@8974 0cb0e2da-76ef-49b7-9531-4241df5af7e9

---
+++ js/topics2015.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#articles_title li nav a img").hover(
+        function(){
+            $(this).html('<img src="./images/ja/topics7/suzuki_on.png" alt="ŒvŽZî•ñŠw‡T, —é–Ø‘×”Ž y‹³Žö, î•ñ•¶‰»Šw•”">');
+            $(this).css({ margin: '10px 0 25px 0' });
+        },
+        function(){
+            $(this).html('<img src="./images/ja/topics7/suzuki_lead.png" alt="ŒvŽZî•ñŠw‡T, —é–Ø‘×”Ž y‹³Žö, î•ñ•¶‰»Šw•”">');
+            $(this).css({ margin: '10px 0 25px 0' });
+        }
+    );
+});
